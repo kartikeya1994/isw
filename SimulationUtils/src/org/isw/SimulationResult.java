29fe7392df8e5b702934bc534e43f35b8f75cd99
Miroojin Bakshi <gaglu231094@gmail.com>
2015-05-18 18:54:34 +0530

    Fix derps

---
+++ SimulationUtils/src/org/isw/SimulationResult.java
@@ -0,0 +1,33 @@
+package org.isw;
+
+public class SimulationResult {
+  double cost;
+  double pmAvgTime;
+  int compCombo;
+  int pmOpportunity;
+  public long t; //to be used for calculations by maintenance dept
+  public int id; //to be used for calculations by maintenance dept
+  public SimulationResult(double cost,double pmAvgTime,int compCombo,int pmOpportunity){
+	  this.cost = cost;
+	  this.pmAvgTime = pmAvgTime;
+	  this.compCombo = compCombo;
+	  this.pmOpportunity = pmOpportunity;
+			 
+  }
+  public double getCost(){
+	  return cost;
+  }
+  public double getPMAvgTime(){
+	  return pmAvgTime;
+  }
+  public int getCompCombo(){
+	  return compCombo;
+  }
+  public int getPMOpportunity(){
+	  return pmOpportunity;
+  }
+public void setCost(double cost) {
+	this.cost = cost;
+	
+}
+}
