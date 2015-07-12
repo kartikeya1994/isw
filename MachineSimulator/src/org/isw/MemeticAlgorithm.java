ee286f708db745051002af31c93024a6ec407591
Miroojin Bakshi <mbakshi1094@gmail.com>
2015-07-12 16:05:07 +0530

    Use arraylist Implement roulette selection Implement single point crossover Implement fitness evaluation

--- MachineSimulator/src/org/isw/MemeticAlgorithm.java
+++ MachineSimulator/src/org/isw/MemeticAlgorithm.java
@@ -1,64 +1,123 @@
 package org.isw;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.commons.math3.distribution.EnumeratedDistribution;
 import org.apache.commons.math3.util.Pair;
+import org.isw.threads.SimulationThread;
 
 public class MemeticAlgorithm {
 	private int populationSize;
 	private int stopCrit;
 	private int pmOpportunity;
 	private Schedule schedule;
-	private Chromosome[] population;
+	private ArrayList<Chromosome> population;
+	private ArrayList<Chromosome> offsprings;
 	private Chromosome bestChromosome;
 	private double totalFitness;
+	Random rand;
+	EnumeratedDistribution<Chromosome> distribution;
 	public MemeticAlgorithm(int populationSize, int stopCrit, Schedule schedule ,int pmOpportunity){
 		this.populationSize = populationSize;
-		this.population = new Chromosome[populationSize];
+		this.population = new ArrayList<Chromosome>();
 		this.stopCrit = stopCrit;
 		this.schedule = schedule;
 		this.pmOpportunity = pmOpportunity;
 		bestChromosome = new Chromosome(0);
+		rand = new Random();
 	}
 	
-	public SimulationResult execute(){
+	public SimulationResult execute() throws InterruptedException, ExecutionException{
 		initializePopulation();
 		int cnt=0;
 		while(true){
 			optimizePopulation();
-			evaluateFitness();
+			evaluateFitness(population);
+			distribution = new EnumeratedDistribution<Chromosome>(populationDistribution());
 			if(cnt++ >= stopCrit)
 				break;
-			Chromosome[] parents = selectParents();
-			generatePopulation(parents);
+			generatePopulation();
 		}
 		return null;
 	}
 
-	private void generatePopulation(Chromosome[] parents) {
-		//TODO
-		
+	private void generatePopulation() throws InterruptedException, ExecutionException {
+	//TODO: Mutation, crossover ratio
+		offsprings = new ArrayList<Chromosome>();
+		int numberOfPairs = (populationSize/4%2==0)?populationSize/4:populationSize/4+1; 
+		for(int i=0;i<numberOfPairs;i++){
+			Chromosome[] parents = selectParents();
+			if(parents[0].combo != parents[1].combo){
+					Chromosome[] offspring = crossover(parents[0],parents[1]);
+					offsprings.add(offspring[0]);	
+					offsprings.add(offspring[1]);	
+					}
+		}
+		evaluateFitness(offsprings);
 	}
+	
+	//Single point crossover
+	private Chromosome[] crossover(Chromosome parent1, Chromosome parent2) {
+		Chromosome[] offsprings = new Chromosome[2];
+		int combo1[] = {parent1.combo,parent1.combo};
+		int combo2[] = {parent2.combo,parent2.combo};
+		
+		int crossoverPoint = rand.nextInt(Machine.compList.length-1) + 1;
+		for(int i=0; i<Machine.compList.length;i++){
+			if(i < crossoverPoint){
+				combo2[0] = combo2[0] & ~(1<<i);
+				combo1[1] = combo2[1] & ~(1<<i);
+			}
+			else{
+				combo1[0] = combo1[0] & ~(1<<i);
+				combo2[1] = combo2[1] & ~(1<<i);
+			}
+		}
+		offsprings[0] = new Chromosome(combo1[0]|combo2[0]);
+		offsprings[1] = new Chromosome(combo1[1]|combo2[1]);
+		return offsprings;
+	}
+
 
 	private Chromosome[] selectParents() {
-		EnumeratedDistribution<Chromosome> distribution = new EnumeratedDistribution<Chromosome>(populationDistribution());
-		return (Chromosome[]) distribution.sample((populationSize/2%2==0)?populationSize/2:populationSize/2+1);
+		return (Chromosome[]) distribution.sample(2);
 	}
-
+	
+	//Distribution for the roulette wheel selection
 	private List<Pair<Chromosome, Double>> populationDistribution() {
 		ArrayList<Pair<Chromosome, Double>> dist = new ArrayList<Pair<Chromosome, Double>>();
 		for(int i=0;i<populationSize;i++){
-			dist.add(new Pair<Chromosome, Double>(population[i],population[i].fitnessValue/totalFitness));
+			dist.add(new Pair<Chromosome, Double>(population.get(i),population.get(i).fitnessValue/totalFitness));
 		}
 		return dist;
 	}
 
-	private void evaluateFitness() {
-		//TODO
-		
+	private void evaluateFitness(ArrayList<Chromosome> list) throws InterruptedException, ExecutionException {
+		HashMap<Integer, Chromosome> map = new HashMap<Integer, Chromosome>(); 
+		ExecutorService threadPool = Executors.newFixedThreadPool(20);
+		CompletionService<SimulationResult> pool = new ExecutorCompletionService<SimulationResult>(threadPool);
+		int cnt = 0 ;
+		for(Chromosome chromosome : list){
+			map.put(chromosome.combo, chromosome);
+			pool.submit(new SimulationThread(schedule,chromosome.combo,pmOpportunity));
+			cnt++;
+		}
+		for(int i=0;i<cnt;i++){
+			SimulationResult result = pool.take().get();
+			map.get(result.compCombo).fitnessValue = result.cost;
+			
+		}
+		threadPool.shutdown();
+		while(!threadPool.isTerminated());
+		 
 	}
 
 	private void optimizePopulation() {
@@ -68,9 +127,9 @@ public class MemeticAlgorithm {
 	}
 
 	private void initializePopulation() {
-		Random rand = new Random();
+		
 		for(int i=0;i<populationSize;i++){
-			  population[i] = new Chromosome(rand.nextInt((int)Math.pow(2, Machine.compList.length)) + 1);
+			  population.add(new Chromosome(rand.nextInt((int)Math.pow(2, Machine.compList.length)) + 1));
 		}
 		
 	}
